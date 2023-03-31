e5170e4ca5efb1c99145dba27c529757dc835c3b
Frynoodles <sanshuidesu@163.com>
2023-04-01 02:30:05 +0800

    添加时间表

--- web.source/src/views/StorageView.vue
+++ web.source/src/views/StorageView.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 </script>
 <template>
-    <div></div>
+    <div><iframe src="../../resources/湖南大学工商管理学院2006届毕业生毕业论文写作进程安排.pdf"/></div>
 </template>
 
 <style scoped lang="less">
+iframe{
+    width: 1200px;
+    height: 800px;
+    position: relative;
+    left: 350px;
+    top: 100px;
+}
 </style>
